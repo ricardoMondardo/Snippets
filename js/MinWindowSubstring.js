d616ab1b13b925ae9187509cdbbc63224dc8e889
Ricardo <ricardo9300@gmail.com>
2021-06-22 01:30:14 -0300

    update
    update

--- js/MinWindowSubstring.js
+++ js/MinWindowSubstring.js
@@ -27,10 +27,22 @@ function MinWindowSubstring(strArr) {
     for (let i = 0; i < arrPhase.length; i++)
     {
         intervalPartial = frequanceInterval;
-        if (intervalPartial[arrPhase[i]] != null) intervalPartial[arrPhase[i]] = -1;
+        if (intervalPartial[arrPhase[i]] != null) intervalPartial[arrPhase[i]] = intervalPartial[arrPhase[i]] -1;
         for (let j = i; j < arrPhase.length; j++)
         {
-            if (intervalPartial[arrPhase[j]] != null) intervalPartial[arrPhase[j]] = -1;
+            if (intervalPartial[arrPhase[j]] != null) {
+                intervalPartial[arrPhase[j]] = intervalPartial[arrPhase[j]] -1;
+                if (intervalPartial[arrPhase[j]] == 0) 
+                {
+                    console.log('aa')
+                }
+                // if () {
+                // check if found all items { j: -1, e: -1, f: -1, a: -1 }
+                
+                // Add a list starting from I ( first found)
+                // until J ( last found)
+                // }
+            }
             console.log(intervalPartial)
         }
         
